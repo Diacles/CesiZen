4d0a10453bcae2b38b0f57171751a9af4b144a54
Alcides <galcides.pepin@gmail.com>
2025-03-16 08:36:34 +0100

    [FIX] Tailwind configuration

--- cesizen-front/tailwind.config.js
+++ cesizen-front/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#00BE62",
-        secondary: "#00E074",
-        accent: "#FFDE59",
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#00BE62",
+          secondary: "#00E074",
+          accent: "#FFDE59",
+        },
       },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
